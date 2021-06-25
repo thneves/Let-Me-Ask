4ae72d794deab9f39405150d6c7e2997ba54d5f2
thneves <tsneves11@gmail.com>
2021-06-25 00:57:14 -0300

    Create and style Room page component

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,33 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import '../styles/room.scss';
+
 export function Room() {
   return (
-    <h1>Room</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="letmeask logo" />
+          <div>room code</div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>React Room</h1>
+          <span>4 questions</span>
+        </div>
+
+        <form>
+          <textarea
+            placeholder="What's your questions?"
+          />
+          <div className="form-footer">
+            <span>To send a question, <button>please, log in.</button></span>
+            <Button type="submit">Send Question</Button>
+          </div>
+        </form>
+      </main>
+    </div>
   )
 }
