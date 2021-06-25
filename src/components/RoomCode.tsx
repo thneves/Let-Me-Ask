d81b9ec79ee35951840273f4a7a7b77295ea65a1
thneves <tsneves11@gmail.com>
2021-06-25 01:15:00 -0300

    Create RoomCode and clipboard function

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,23 @@
+import CopyImg from '../assets/images/copy.svg';
+
+import '../styles/room-code.scss';
+
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code)
+  }
+
+  return (
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
+      <div>
+        <img src={CopyImg} alt="Copy room code" />
+      </div>
+      <span> Room {props.code}</span>
+    </button>
+  )
+}
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,24 @@
 import logoImg from '../assets/images/logo.svg';
+import { useParams } from 'react-router-dom';
 import { Button } from '../components/Button';
 import '../styles/room.scss';
+import { RoomCode } from '../components/RoomCode';
+
+
+type RoomParams = {
+  id: string;
+}
 
 export function Room() {
+
+  const params = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="letmeask logo" />
-          <div>room code</div>
+          <RoomCode code={params.id} />
         </div>
       </header>
 
