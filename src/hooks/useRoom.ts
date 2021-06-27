f8fbe8997b5c89e474512c513282f53e3ebf8bfd
thneves <tsneves11@gmail.com>
2021-06-27 20:20:52 -0300

    Implement first Like button functionality

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type FirebaseQuestions = Record<string, {
   author: {
@@ -9,6 +10,9 @@ type FirebaseQuestions = Record<string, {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likes: Record<string, {
+    authorId: string;
+  }>
 }>
 
 type QuestionType = {
@@ -21,17 +25,22 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likeCount: number,
+  hasLiked: boolean,
 }
 
 
 export function useRoom (roomId: string) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([])
   const [title, setTitle] = useState('');
   
   useEffect(() => {
+
     const roomRef = database.ref(`rooms/${roomId}`);
 
     roomRef.on('value', room => {
+
       const databaseRoom = room.val();
       const firebaseQuestions = databaseRoom.questions as FirebaseQuestions ?? {};
 
@@ -42,13 +51,19 @@ export function useRoom (roomId: string) {
           author: value.author,
           isHighlighted: value.isHighlighted,
           isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id)
         }
       })
 
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
     })
-  }, [roomId]);
+
+    return () => {
+      roomRef.off('value') // Removing updates
+    }
+  }, [roomId, user?.id]);
 
   return { questions, title }
 } 
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -3,9 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Button } from '../components/Button';
 import '../styles/room.scss';
 import { RoomCode } from '../components/RoomCode';
-import { FormEvent, useState } from 'react';
-import { useAuth } from '../hooks/useAuth';
-import { database } from '../services/firebase';
+// import { useAuth } from '../hooks/useAuth';
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
 
@@ -15,39 +13,12 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
-  const { user } = useAuth();
+  // const { user } = useAuth();
   const params = useParams<RoomParams>();
-  const [newQuestion, setNewQuestion] = useState('');
 
   const roomId = params.id;
 
-  const { title, questions } = useRoom(roomId); 
- 
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault();
-
-    if (newQuestion.trim() === '') {
-      return;
-    }
-
-    if (!user) {
-      throw new Error('You mus be logged in.')
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar,  
-      },
-      isHighLightned: false,
-      isAnswered: false  // Determine if the question was answered or not.
-    }
-
-    await database.ref(`rooms/${roomId}/questions`).push(question)
-
-    setNewQuestion('');
-  }
+  const { title, questions } = useRoom(roomId);
 
   return (
     <div id="page-room">
