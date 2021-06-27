3ea7131dfa2d9946ca0785448c7dcd71934c98bf
thneves <tsneves11@gmail.com>
2021-06-27 18:45:22 -0300

    Create and style Like button

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import '../styles/question.scss';
 
 type QuestionProps = {
@@ -5,10 +6,11 @@ type QuestionProps = {
   author: {
     name: string;
     avatar: string;
-  }
+  };
+  children?: ReactNode;
 }
 
-export function Question ({content, author}: QuestionProps) {
+export function Question ({content, author, children}: QuestionProps) {
 
   return (
     <div className="question">
@@ -18,7 +20,9 @@ export function Question ({content, author}: QuestionProps) {
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>
+          {children}
+        </div>
       </footer>
     </div>
   );
